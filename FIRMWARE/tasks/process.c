88ab2d98fd8fac5e2e41577c9993b641b006dc03
Khasin, Ark <ark.khasin@BakerHughes.com>
2023-01-06 18:09:03 +0000

    A partial solution to 89378 did too much because of side effects of buffer_StopSampling. This introduced bugs 89512 and 89513. buffer_StopSampling is replaced with buffer_SuspendSampling
    git-tfs-id: [http://szvptemfou01:8080/tfs/DefaultCollection]$/FW SVI FF Releases/Release3/FIRMWARE;C97185

--- FIRMWARE/tasks/process.c
+++ FIRMWARE/tasks/process.c
@@ -536,8 +536,11 @@ procresult_t process_Exec(ProcId_t procId)
         //Notify UI that no process is started from UI
         process_SetRequestorFlag(0);
 
-        //Unconditionally stop sampling
-        buffer_StopSampling(DIAGBUF_DEFAULT);
+        {
+            //Unconditionally stop sampling but without here-irrelevant housekeeping
+            u16 skip = buffer_SuspendSampling(DIAGBUF_DEFAULT);
+            UNUSED_OK(skip); //we aint gonna resume
+        }
     }
 
     return result;
