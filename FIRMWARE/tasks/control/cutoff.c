f41ee59029ac4a53f0f4f9efb2d0ca4c2bbf01fc
Haldiya, Udit <Udit.Haldiya@bakerhughes.com>
2025-02-10 05:42:37 +0000

    Missed one revert
    git-tfs-id: [http://szvptemfou01:8080/tfs/DefaultCollection]$/FW SVI FF Releases/Release3/FIRMWARE;C102899

--- FIRMWARE/tasks/control/cutoff.c
+++ FIRMWARE/tasks/control/cutoff.c
@@ -79,7 +79,7 @@ bool_t cutoff_IsActive(void)
     \param indicator of closed-loop control
     \return  true iff cutoff (either low or high)
 */
-static bool_t cutoff_Eval(bool_t closed_loop)
+bool_t cutoff_Eval(bool_t closed_loop)
 {
     CtlLimits_t         ctlLimits;
 
